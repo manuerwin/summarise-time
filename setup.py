091140549a8c8b5e32794b9259a98497972dfe51
Manu Erwin <manuerwin@gmail.com>
2025-05-04 15:12:25 +1200

    chore: setup package

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools as sut
+
+sut.setup(
+    name="Summarise Activities",
+    version="1.0.0",
+    description="Python app - process and summarise activity lines from CSV",
+    author="Manu Erwin",
+    packages=sut.find_packages(),
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+    ],
+    include_package_data=True,
+)
